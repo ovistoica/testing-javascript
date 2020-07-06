3d9be3c164a1720fe6d7b1f4b01b242625621713
stoica94 <ovidiu.stoica1094@gmail.com>
2020-07-06 09:08:34 +0300

    Add jest-dom to get more specific info on tests about dom nodes

--- lessons/3-configuring-jest/jest.config.js
+++ lessons/3-configuring-jest/jest.config.js
@@ -1,8 +1,11 @@
+const path = require('path')
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
+  moduleDirectories: ['node_modules', path.join(__dirname, 'src'), 'shared'],
   moduleNameMapper: {
     '\\.module\\.css': 'identity-obj-proxy',
     '\\.css$': require.resolve('./test/style-mock.js'),
   },
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   snapshotSerializers: ['jest-emotion'],
 }
---
+++ lessons/3-configuring-jest/src/__tests__/calculator.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {render, fireEvent} from '@testing-library/react'
+import Calculator from '../calculator'
+import {findRenderedDOMComponentWithClass} from 'react-dom/test-utils'
+
+test('The clear button switches from AC to C when there is an entry', () => {
+  const {getByText} = render(<Calculator />)
+  const clearButton = getByText('AC')
+
+  fireEvent.click(getByText(/3/))
+  expect(clearButton).toHaveTextContent('C')
+
+  fireEvent.click(clearButton)
+  expect(clearButton).toHaveTextContent('AC')
+})
