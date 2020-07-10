8dc831c898140ac1c66cc5b46076291d51bc19d3
stoica94 <ovidiu.stoica1094@gmail.com>
2020-07-10 18:00:54 +0300

    Have tests for accesibility

--- lessons/4-testing-react-components/src/__tests__/prop-updates.js
+++ lessons/4-testing-react-components/src/__tests__/prop-updates.js
@@ -3,14 +3,18 @@ import user from '@testing-library/user-event'
 import {render} from '@testing-library/react'
 import {FavoriteNumber} from '../favorite-number'
 
+/**
+ * Use `getByRole` when the dom node exists and `queryByRole` when
+ * you expect the DOM node not to exist
+ */
+
 test('entering an invalid value shows an error message', () => {
-  const {getByLabelText, getByRole, rerender, debug} = render(
+  const {getByLabelText, getByRole, rerender, queryByRole} = render(
     <FavoriteNumber />,
   )
   const input = getByLabelText(/favorite number/i)
   user.type(input, '10')
   expect(getByRole('alert')).toHaveTextContent(/the number is invalid/i)
-  debug()
   rerender(<FavoriteNumber max={10} />)
-  debug()
+  expect(queryByRole('alert')).toBeNull()
 })
--- lessons/4-testing-react-components/src/__tests__/state.js
+++ lessons/4-testing-react-components/src/__tests__/state.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import user from '@testing-library/user-event'
-import {render} from '@testing-library/react'
+import {render, fireEvent} from '@testing-library/react'
 import {FavoriteNumber} from '../favorite-number'
 
 test('entering an invalid value shows error', () => {
   const {getByLabelText, getByRole} = render(<FavoriteNumber />)
   const input = getByLabelText(/favorite number/i)
-  user.type(input, '10')
+  fireEvent.change(input, {target: {value: '10'}})
   expect(getByRole('alert')).toHaveTextContent(/the number is invalid/i)
 })
